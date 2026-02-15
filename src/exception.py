bcba602be4d0917aacba4c0828d14912b8b38631
nastaran <nezamivand.c@gmail.com>
2026-02-15 13:06:15 +0330

    EDA and Problem state

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-
+from src.logger import logging
 
 def error_message_detail(error,error_detail:sys):
     _,_,exc_tb = error_detail.exc_info()
@@ -15,7 +15,6 @@ class customException(Exception):
           
     def __str__(self):
         return self.error_message 
-    
-  
-        
+   
+
     
